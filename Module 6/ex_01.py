8019bda991157238028b651fd27845016c0035a1
XEvan-WiseX <116358021+XEvan-WiseX@users.noreply.github.com>
2023-03-06 13:33:25 -0500

    Add files via upload

---
+++ Module 6/ex_01.py
@@ -0,0 +1,9 @@
+str = 'abcde'
+
+countdown = len(str) - 1
+
+while countdown >= 0:
+    printout = str[countdown]
+    print(printout)
+    countdown = countdown - 1
+    
---
+++ Module 6/ex_06.py
@@ -0,0 +1,7 @@
+print("The sytax is still really confusing to me.  I am starting to be more familiar with it, but I will definitely need to practice more.  It seems like it was all created for specific tasks rather than a overarching goal.  For example, a lot of the individual functions seem to be for mass processing of user created text documents like microsoft word files.")
+
+string = input("Enter Input: ")
+
+start = string.find('ion')
+
+print(start + 1)
